cf4c902044537d2ec3518a88a8f1b28ee3eb1ae4
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-11-03 23:28:47 +0900

    Revert "wip"
    This reverts commit 2497c69a1cba2ba2e768513429cc9141d6f7f763.

--- src/index.tsx
+++ src/index.tsx
@@ -1,42 +1,8 @@
-import React, {
-  useRef,
-  useState,
-  MouseEventHandler,
-  useLayoutEffect
-} from "react"
+import React from "react"
 import { render } from "react-dom"
-import { MaskCanvas } from "./draw/MaskCanvas"
-import styled from "styled-components"
-import { StringSample } from "./ResultOutput"
-import { Title } from "./Title"
-const Input = styled.input`
-  padding: 0.5em;
-  font-size: 1.5em;
-`
-const Body = styled.div`
-  box-sizing: border-box;
-`
 
 const App = () => {
-  const [mask, setMask] = useState(null)
-  const [text, setText] = useState("ショッパーズ")
-  // const [width, setWidth] = useState(0)
-  const fontSize = "2em"
-  return (
-    <Body>
-      <Title />
-      <Input value={text} onChange={setText} />
-
-      <MaskCanvas
-        fontSize={fontSize}
-        text={text}
-        onChangeMask={(cnvMask) => setMask(cnvMask)}
-      ></MaskCanvas>
-      <StringSample mask={mask} fontSize={fontSize}>
-        {text}
-      </StringSample>
-    </Body>
-  )
+  return <div>hello</div>
 }
 
 render(<App />, document.querySelector("#root"))
